8769d8aebeb7fec71a7ac117f30d8d08ab3e1287
Miro Saastamoinen <miromnb@icloud.com>
2026-04-02 10:42:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8356187000-f0a5f",
+  "appId": "1:1084571283377:web:3b59936f487eaab43b9790",
+  "apiKey": "AIzaSyDXi69s4tyN-ijQzNc0UWkH303img3jo_s",
+  "authDomain": "studio-8356187000-f0a5f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1084571283377"
+};
