df67a4e701408fa5c4ab4defe75ec83d91829007
Miro Saastamoinen <miromnbmnbmnb@gmail.com>
2026-04-06 13:58:12 +0000

    Miksi et pysty tekemään tätä mikä kuvassa näkyy

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,6 @@ export const firebaseConfig = {
   authDomain: process.env.NEXT_PUBLIC_VITE_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_VITE_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_VITE_FIREBASE_MESSAGING_SENDER_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_VITE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_VITE_FIREBASE_APP_ID
 };
