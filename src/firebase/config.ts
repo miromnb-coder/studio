1310058ef6682b818d0e97f20a9b400d237dc18d
Miro Saastamoinen <miromnbmnbmnb@gmail.com>
2026-04-06 14:19:19 +0000

    Laita sinä ne minun puolesta const firebaseConfig = {   apiKey: "AIzaSyD

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || process.env.NEXT_PUBLIC_VITE_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || process.env.NEXT_PUBLIC_VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || process.env.NEXT_PUBLIC_VITE_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || process.env.NEXT_PUBLIC_VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || process.env.NEXT_PUBLIC_VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || process.env.NEXT_PUBLIC_VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyDXi69s4tyN-ijQzNc0UWkH303img3jo_s",
+  authDomain: "studio-8356187000-f0a5f.firebaseapp.com",
+  projectId: "studio-8356187000-f0a5f",
+  storageBucket: "studio-8356187000-f0a5f.firebasestorage.app",
+  messagingSenderId: "1084571283377",
+  appId: "1:1084571283377:web:3b59936f487eaab43b9790"
 };
