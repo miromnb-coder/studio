1fbe32f519408c2e7689d013625a204219c502b2
miromnb-coder <miromnb@icloud.com>
2026-04-25 16:10:23 +0300

    Refactor manifest function in manifest.ts

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,8 +1,4 @@
 
-index 4d5df5655ca7e17d837fa382230f8069707c2758..af1ec907a74173ce57e96b7b067d87c2d4b6b97d 100644
---- a/src/app/manifest.ts
-+++ b/src/app/manifest.ts
-@@ -1,30 +1,30 @@
  import type { MetadataRoute } from 'next';
  
  export default function manifest(): MetadataRoute.Manifest {
